e5b7084b8ccc310a20102361b4e8bf026c648628
Necip Allef <necipallef@gmail.com>
2022-09-12 14:01:07 +0300

    try fpjs

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {
+  FingerprintJsProProvider,
+  useVisitorData,
+} from '@fingerprintjs/fingerprintjs-pro-react-native';
+
+function Screen1() {
+  const {isLoading, error, data, getData} = useVisitorData();
+
+  useEffect(() => {
+    //getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (isLoading) {
+    return <Text>loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>error: {error.message}</Text>;
+  }
+
+  if (data) {
+    return <Text>{JSON.stringify(data)}</Text>;
+  }
+
+  return <Text>unexpected</Text>;
+}
+
+function Screen2() {
+  return <Text>screen 2</Text>;
+}
 
 export function App() {
   return (
-    <SafeAreaView>
-      <Text>Hello from App</Text>
-    </SafeAreaView>
+    <FingerprintJsProProvider apiKey={'ztukooZ3oRmG8FgOXMva'}>
+      <SafeAreaView>
+        {/*<Screen1 />*/}
+        <Screen2 />
+      </SafeAreaView>
+    </FingerprintJsProProvider>
   );
 }
