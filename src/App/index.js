14785d9bd7a89e07b04684767115d2e8b0e78489
Necip Allef <necipallef@gmail.com>
2022-09-12 13:00:23 +0300

    initial commit

---
+++ src/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello from App</Text>
+    </SafeAreaView>
+  );
+}
